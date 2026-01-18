96780a7891f8509ccb2933b861c808b52ba3fa20
המאיר לארץ <ameirlaarez@gmail.com>
2026-01-18 22:00:37 +0200

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,28 +1,22 @@
+const PREFIX = 'stbl_';
+
 export const DB = {
-  prefix: 'sk_v2_',
-
-  get(key) {
-    return JSON.parse(localStorage.getItem(this.prefix + key)) || [];
-  },
-
-  set(key, value) {
-    localStorage.setItem(this.prefix + key, JSON.stringify(value));
-  },
-
   init() {
-  if (!localStorage.getItem(this.prefix + 'products')) {
-    this.set('products', [
-      { id: 1, name: 'מוצר בדיקה', stock: 10, cost: 20, price: 40 }
-    ]);
-  }
+    if (!localStorage.getItem(PREFIX + 'stats')) {
+      localStorage.setItem(
+        PREFIX + 'stats',
+        JSON.stringify({
+          products: 3,
+          suppliers: 2,
+          revenue: 1200
+        })
+      );
+    }
+  },
 
-  if (!localStorage.getItem(this.prefix + 'suppliers')) {
-    this.set('suppliers', [
-      { id: 1, name: 'ספק בדיקה', balance: 200 }
-    ]);
+  getStats() {
+    return JSON.parse(
+      localStorage.getItem(PREFIX + 'stats')
+    );
   }
-
-  if (!localStorage.getItem(this.prefix + 'stats')) {
-    this.set('stats', { revenue: 500, profit: 300 });
-  }
-}
+};
