1bc44be4f2293e4364a3eda816d045e6e0426d24
המאיר לארץ <ameirlaarez@gmail.com>
2026-01-18 21:44:52 +0200

    Update suppliers.js

--- js/suppliers.js
+++ js/suppliers.js
@@ -1,1 +1,9 @@
+import { DB } from './db.js';
+
+export function renderSuppliers(container) {
+  const suppliers = DB.get('suppliers');
+  container.innerHTML = suppliers.map(s=>`
+    <div class="card mb-2">${s.name} – חוב ₪${s.balance}</div>
+  `).join('');
+}
 
