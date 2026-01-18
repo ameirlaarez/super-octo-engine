b5c41eea20dd6bffc320de39491462d42c549e13
המאיר לארץ <ameirlaarez@gmail.com>
2026-01-18 21:46:00 +0200

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,10 @@
+export function showModal(html) {
+  document.getElementById('modal-content').innerHTML = html;
+  document.getElementById('modal-overlay').classList.remove('hidden');
+  document.getElementById('modal-overlay').classList.add('flex');
+}
+
+export function closeModal() {
+  document.getElementById('modal-overlay').classList.add('hidden');
+}
 
