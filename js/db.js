ea5f70706f786327ad01fe594f5b75b8ddcdbc09
המאיר לארץ <ameirlaarez@gmail.com>
2026-01-18 22:07:19 +0200

    Update db.js

--- js/db.js
+++ js/db.js
@@ -3,20 +3,15 @@ const PREFIX = 'stbl_';
 export const DB = {
   init() {
     if (!localStorage.getItem(PREFIX + 'stats')) {
-      localStorage.setItem(
-        PREFIX + 'stats',
-        JSON.stringify({
-          products: 3,
-          suppliers: 2,
-          revenue: 1200
-        })
-      );
+      localStorage.setItem(PREFIX + 'stats', JSON.stringify({
+        products: 3,
+        suppliers: 2,
+        revenue: 1200
+      }));
     }
   },
 
   getStats() {
-    return JSON.parse(
-      localStorage.getItem(PREFIX + 'stats')
-    );
+    return JSON.parse(localStorage.getItem(PREFIX + 'stats'));
   }
 };
