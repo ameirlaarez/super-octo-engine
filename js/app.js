a65f9d160c3e545492642d86f32631965d035040
המאיר לארץ <ameirlaarez@gmail.com>
2026-01-18 21:35:44 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
+import './state.js';
+import './db.js';
+import './auth.js';
+import './dashboard.js';
+import './suppliers.js';
+import './products.js';
+import './payments.js';
+import './reports.js';
+import './ui.js';
+
+showSection('dashboard');
 
