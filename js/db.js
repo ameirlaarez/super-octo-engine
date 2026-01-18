2993da8ee788e41a333fc340bcd6b494b92d4fe7
המאיר לארץ <ameirlaarez@gmail.com>
2026-01-18 21:40:39 +0200

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,1 +1,21 @@
+export const DB = {
+  prefix: 'sk_v2_',
+
+  get(key) {
+    return JSON.parse(localStorage.getItem(this.prefix + key)) || [];
+  },
+
+  set(key, value) {
+    localStorage.setItem(this.prefix + key, JSON.stringify(value));
+  },
+
+  init() {
+    ['products','suppliers','history'].forEach(k=>{
+      if(!localStorage.getItem(this.prefix+k)) this.set(k,[]);
+    });
+    if(!localStorage.getItem(this.prefix+'stats')) {
+      this.set('stats',{ revenue:0, profit:0 });
+    }
+  }
+};
 
